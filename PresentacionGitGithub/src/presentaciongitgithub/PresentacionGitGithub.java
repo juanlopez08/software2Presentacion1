ce3a878579dfa643c66785a126f174a58ff46d6f
Magody <meliodasfistery@gmail.com>
2019-10-20 18:17:14 -0500

    Agregación de la pantalla funcionando
    Ya calcula si una cédula es válida o no

--- PresentacionGitGithub/src/presentaciongitgithub/PresentacionGitGithub.java
+++ PresentacionGitGithub/src/presentaciongitgithub/PresentacionGitGithub.java
@@ -15,7 +15,8 @@ public class PresentacionGitGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
     }
     
 }
